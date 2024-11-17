61aa583549169e39b654982f7aeb1934f2df6248
abhishekkulkarni59 <abhishekkulkarni59@gmail.com>
2024-11-17 20:33:54 +0530

    Database connection through jpa

---
+++ taletrail/src/main/java/com/code/taletrail/TaletrailApplication.java
@@ -0,0 +1,13 @@
+package com.code.taletrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaletrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaletrailApplication.class, args);
+	}
+
+}
---
+++ taletrail/src/test/java/com/code/taletrail/TaletrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.taletrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaletrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
