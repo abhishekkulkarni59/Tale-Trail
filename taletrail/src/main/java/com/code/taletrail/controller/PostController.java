5536a82ca6de96884aa2e913f9559905e6dd9f99
abhishekkulkarni59 <abhishekkulkarni59@gmail.com>
2024-12-08 23:22:20 +0530

    Image Uploading api added for Posts.

--- taletrail/src/main/java/com/code/taletrail/controller/PostController.java
+++ taletrail/src/main/java/com/code/taletrail/controller/PostController.java
@@ -3,13 +3,21 @@ package com.code.taletrail.controller;
 import com.code.taletrail.config.AppConstants;
 import com.code.taletrail.payload.PostDto;
 import com.code.taletrail.payload.PostResponse;
+import com.code.taletrail.service.FileService;
 import com.code.taletrail.service.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +27,12 @@ public class PostController {
 
     @Autowired
     private PostService postService;
-    private List<PostDto> posts;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     //GET
     @GetMapping("/{postId}")
@@ -32,7 +45,7 @@ public class PostController {
     public ResponseEntity<PostResponse> getAllPosts(@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
                                                     @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
                                                     @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
-                                                    @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir) {
+                                                    @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR , required = false) String sortDir) {
         PostResponse postResponse = postService.getAllPosts(pageNumber, pageSize, sortBy, sortDir);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
@@ -80,5 +93,23 @@ public class PostController {
 
     }
 
+    //POST IMAGE UPLOAD
+    @PostMapping("/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(@RequestParam("image")MultipartFile image,
+                                                         @PathVariable Integer postId) throws IOException {
+        String fileName = fileService.uploadImage(path, image);
+        PostDto postDto = postService.getPostById(postId);
+        postDto.setImageName(fileName);
+        PostDto updatedPost = postService.updatePost(postDto, postId);
+        return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+    }
 
+    //GET IMAGE
+    @GetMapping(value="/image/{imageName}", produces= MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable String imageName,
+                              HttpServletResponse response) throws IOException {
+        InputStream resource = fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
+    }
 }
